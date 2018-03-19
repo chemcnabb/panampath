3aa73d9e9a1c23e05eab372bc06c6be561315314
Che Daniel Mcnabb <che.mcnabb@sgsco.com>
2018-03-19 14:38:34 -0400

    fixing settings

--- panamsite/urls.py
+++ panamsite/urls.py
@@ -17,6 +17,8 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from home.views import IndexView
 from django.views.generic import TemplateView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
@@ -24,4 +26,4 @@ urlpatterns = [
     url(r'^story/', TemplateView.as_view(template_name='story.html')),
     url(r'^blog/', include('zinnia.urls')),
     url(r'^comments/', include('django_comments.urls')),
-]
+] +  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
