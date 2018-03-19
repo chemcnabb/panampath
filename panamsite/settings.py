4afec44a9e3c7d0863fbb9f320125b07533cd403
Che Daniel Mcnabb <che.mcnabb@sgsco.com>
2018-03-19 14:29:03 -0400

    fixing settings

--- panamsite/settings.py
+++ panamsite/settings.py
@@ -127,7 +127,7 @@ USE_L10N = True
 
 USE_TZ = False
 
-ZINNIA_UPLOAD_TO = "/static/uploads"
+ZINNIA_UPLOAD_TO = "/public/static/uploads"
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, "public/media")
