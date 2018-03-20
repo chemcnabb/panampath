38301fa1852f6cb41569921e09e6c1f2ea6345b0
Che Daniel Mcnabb <che.mcnabb@sgsco.com>
2018-03-20 02:15:15 -0400

    fixing settings

--- home/models.py
+++ home/models.py
@@ -2,5 +2,20 @@
 from __future__ import unicode_literals
 
 from django.db import models
-
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+import subprocess
+from django.conf import settings
 # Create your models here.
+
+
+
+
+
+
+
+# method for updating
+@receiver(post_save)
+def touch_tmp(sender, instance, **kwargs):
+     # print settings.BASE_DIR + "/tmp/restart.txt"
+     subprocess.call(["touch", settings.BASE_DIR + "/tmp/restart.txt"])
