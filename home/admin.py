3e49d6aabf88ec40cf42b1bd70bd31cab8804bd5
Che Daniel Mcnabb <che.mcnabb@sgsco.com>
2018-04-10 19:31:45 -0400

    adding flatpages

--- home/admin.py
+++ home/admin.py
@@ -4,3 +4,17 @@ from __future__ import unicode_literals
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from django.contrib.flatpages.admin import FlatPageAdmin
+from django.contrib.flatpages.models import FlatPage
+from django.db import models
+
+from ckeditor.widgets import CKEditorWidget
+
+class FlatPageCustom(FlatPageAdmin):
+    formfield_overrides = {
+        models.TextField: {'widget': CKEditorWidget}
+    }
+
+admin.site.unregister(FlatPage)
+admin.site.register(FlatPage, FlatPageCustom)
