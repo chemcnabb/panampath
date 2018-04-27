287de5ee8032a9070526926dc29b7e8939a77838
Che Daniel Mcnabb <che.mcnabb@sgsco.com>
2018-04-27 18:07:00 -0400

    adding mapping

--- panamsite/settings.py
+++ panamsite/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+'mapwidgets',
     'django.contrib.flatpages',
     'django_comments',
     'mptt',
