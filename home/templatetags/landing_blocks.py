a997f17eed0136b4c47b30c54e5a57d7b1079705
Che Daniel Mcnabb <che.mcnabb@sgsco.com>
2018-04-29 16:05:35 -0400

    papath notes

--- home/templatetags/landing_blocks.py
+++ home/templatetags/landing_blocks.py
@@ -1,15 +1,25 @@
 from django import template
 from zinnia.models import Category
 register = template.Library()
-
+from panampath.models import PathSegment
 
 @register.inclusion_tag('tags/home_block.html', takes_context=True)
 def home_block(context, category, class_style=""):
     """
     Return the featured entries.
     """
+    return_dict = {'category': category, "class_style": class_style}
+    segments = PathSegment.objects.all()
+    coords = []
+    for segment in segments:
+        for member in segment.geom['coordinates']:
+            for item in member:
+                coords.append(item[::-1])
+    # pprint(coords)
+    return_dict["coordinates"] = coords
     events = None
     if category.lower() == "events":
         events = Category.published.filter(title__in=['Events'])
         events = events[0].entries_published()
-    return {'category': category, "class_style": class_style, "events":events}
+        return_dict["events"] = events
+    return return_dict
--- home/views.py
+++ home/views.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.generic import TemplateView
-
+from panampath.models import PathSegment
 
 # Create your views here.
 class IndexView(TemplateView):
@@ -14,7 +14,15 @@ class IndexView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super(IndexView, self).get_context_data(**kwargs)
-
+        segments = PathSegment.objects.all()
+        coords = []
+        for segment in segments:
+            for member in segment.geom['coordinates']:
+                for item in member:
+                    coords.append(item[::-1])
+        # pprint(coords)
+        context["segments"] = segments
+        context["coordinates"] = coords
 
 
         return context
