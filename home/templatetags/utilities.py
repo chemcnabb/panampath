a09bcc66507f05fb13f1c7f472b66ad443ce3e27
Che Daniel Mcnabb <che.mcnabb@sgsco.com>
2018-06-20 21:05:18 -0400

    adding ordered Nav

--- home/templatetags/utilities.py
+++ home/templatetags/utilities.py
@@ -5,6 +5,9 @@ from zinnia.models import Entry
 from _calendar import Calendar
 
 from datetime import date
+from zinnia.models.category import Category
+from django.db.models import Count
+
 register = template.Library()
 
 
@@ -13,3 +16,30 @@ register = template.Library()
 def clean_word(word):
     word = ''.join(ch for ch in word if ch.isalnum())
     return word.replace(" ", "").strip().lower()
+
+
+
+@register.inclusion_tag('zinnia/tags/dummy.html', takes_context=True)
+def get_categories_override(context, template='zinnia/tags/categories.html'):
+    """
+    Return the published categories.
+    """
+
+    # cats = Category.published.all().annotate(
+    #             count_entries_published=Count('entries'))
+    # MAP
+    # WHAT'S
+    # HAPPENING
+    # PROJECTS
+    # PARTNERSHIPS
+    # IN
+    # THE
+    # NEWS
+    # STORIES
+    # ABOUT
+    cats = Category.published.filter(slug="news").all() | Category.published.filter(slug="projects").all()
+
+    print cats
+    return {'template': template,
+            'categories': cats,
+            'context_category': context.get('category')}
